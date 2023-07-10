5919dfd981bd2ff4e2eb6a876748ebb578740057
Bharath Kumar <bharathkumar05599@gmail.com>
2023-07-10 10:42:35 +0530

    added token generation logic

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -25,6 +25,26 @@ const create = async (req,res) => {
     }
 }
 
+const signin = async (req,res) => {
+    try {
+         const response = await userService.signin(req.body.email,req.body.password);
+         return res.status(201).json({
+            success: true,
+            message: 'succesfully signed in',
+            data: response,
+            error: {}
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: 'Something went wrong',
+            data:{},
+            success: false,
+            error: error
+        })
+    }
+}
 module.exports = {
-    create
+    create,
+    signin
 }
--- src/repositories/user-repository.js
+++ src/repositories/user-repository.js
@@ -38,6 +38,18 @@ class UserRepository {
         }
 
     }
+
+    async getByEmail(userEmail){
+        try {
+            const user = await User.findOne({where: {
+                email: userEmail
+            }})
+            return user;
+        } catch (error) {
+            console.log("Something went wrong on repository layer");
+            throw error;
+        }
+    }
 }
 
 module.exports = UserRepository;
